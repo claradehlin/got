3f5eb693f1e68101aea7d3ef6d6e4fedf98dc337
Clara Dehlin <claramargaretdehlin@gmail.com>
2022-05-25 12:56:18 -0600

    fixed everything because i messed up so bad lmao

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
+
+// Jon's stats
 let jonSnowAttack = 25;
-let jamieLannisterAttack = 35;
+let jonSnowHealth = 100;
+let jonSnowDefense = 0;
+
+// Jamie's stats
+let jamieLannisterAttack = 107;
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow has better attack than Jamie Lannister");
-} else {
+} else if (jonSnowAttack < jamieLannisterAttack){
     console.log("Jamie Lannister has better attack than John Snow");
+} else {
+    console.log("John Snow and Jamie Lannister have the same attack")
 }
+
+if (jonSnowHealth <= jamieLannisterAttack) {
+    console.log("Jon Snow has been slain")
+} else {
+    //jonSnowHealth = jonSnowHealth - jamieLannisterAttack;
+    jonSnowHealth -= jamieLannisterAttack;
+    console.log("Jon Snow's health is down to" + (jonSnowHealth))
+}
+
+// Jon picks up a shield
+jonSnowDefense += 25;
+
+if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
+    console.log("Jon Snow has been slain");
+} else {
+    jonSnowHealth -= (jamieLannisterAttack - jonSnowDefense);
+    console.log("Jon Snow's health is down to " + (jonSnowHealth))
+}
+
+// Peasant throws Jon a health kit of 50: heal up!
+const healthKit = 50;
+
+if ((jonSnowHealth + healthKit) >= 100) {
+    jonSnowHealth = 100;
+  } else {
+    jonSnowHealth += healthKit;
+  }
